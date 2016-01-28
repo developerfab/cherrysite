d154a91d06829497368d31aec313717980150919
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2016-01-28 01:41:07 -0500

    primer commit

---
+++ correo.py
@@ -0,0 +1,23 @@
+import smtplib
+
+def mensaje(msg):
+    print msg
+    fromaddr = 'parapruebasfab@gmail.com'
+    toaddrs  = 'fab7696650@gmail.com'
+    # Credentials (if needed)
+    username = 'parapruebasfab@gmail.com'
+    password = ''
+    mensaje = '''\\
+    From: parapruebasfab@gmail.com
+    Subject: contacto
+
+    '''
+    mensaje = mensaje+"\n"+msg
+
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, mensaje)
+    server.close()
---
+++ flores.py
@@ -0,0 +1,63 @@
+import cherrypy
+import os.path
+import sqlite3
+import correo
+from jinja2 import Environment, FileSystemLoader
+from auth import AuthController, require, member_of, name_is
+
+DB_STRING = "baseflores.py"
+env = Environment(loader=FileSystemLoader('templates'))
+
+class EditContent:
+    _cp_config = {
+        'auth.require': [member_of('admin')]
+    }
+
+class Root(object):
+
+    auth = AuthController()
+    restricted = EditContent()
+
+    @cherrypy.expose
+    def index(self):
+        tmpl = env.get_template('index.html')
+        return tmpl.render(title='Inicio', select='inicio')
+
+    @cherrypy.expose
+    def nosotros(self):
+        tmpl = env.get_template('nosotros.html')
+        return tmpl.render(title='Nosotros', select='nosotros')
+
+    @cherrypy.expose
+    def contacto(self):
+        tmpl = env.get_template('contacto.html')
+        return tmpl.render(title='Contactanos', select='contacto')
+
+    @cherrypy.expose
+    def envio_mensaje(self, **params):
+        dic = cherrypy.request.body.params
+        msg = "Nombre: "+dic['first_name']+" "+dic['last_name']+"\n telefono: "+dic['telephone']+"\n correo "+dic['email']+"\n"+dic['message']
+        correo.mensaje(msg)
+        tmpl = env.get_template('contacto.html')
+        return tmpl.render(title='Contactanos', select='contacto', mensaje=True)
+
+    @require()
+    @cherrypy.expose
+    def edita_nosotros(self):
+        return """Esta es un area restringda."""
+
+    @cherrypy.expose
+    def logeo():
+        return auth.get_loginform()
+
+if __name__ == '__main__':
+    cherrypy.quickstart(Root(), config={
+        '/css':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir': "/Users/fabricio/Desktop/jardin de las flores/sitio_cherry/static/css/"
+        },
+        '/img':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir': "/Users/fabricio/Desktop/jardin de las flores/sitio_cherry/static/img/"
+        },
+    })
